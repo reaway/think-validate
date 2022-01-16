53756ab3c1cbd0dcc15dad7fbd87d9375ea88b63
reaway <reaway@foxmail.com>
2022-01-16 17:34:36 +0800

    init

---
+++ src/helper.php
@@ -0,0 +1,48 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2021 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+declare (strict_types=1);
+
+use Think\Component\Validate\Validate;
+
+if (!function_exists('validate')) {
+    /**
+     * 生成验证对象
+     * @param string|array $validate      验证器类名或者验证规则数组
+     * @param array        $message       错误提示信息
+     * @param bool         $batch         是否批量验证
+     * @param bool         $failException 是否抛出异常
+     * @return Validate
+     */
+    function validate($validate = '', array $message = [], bool $batch = false, bool $failException = true): Validate
+    {
+        if (is_array($validate) || '' === $validate) {
+            $v = new Validate();
+            if (is_array($validate)) {
+                $v->rule($validate);
+            }
+        } else {
+            if (strpos($validate, '.')) {
+                // 支持场景
+                [$validate, $scene] = explode('.', $validate);
+            }
+
+            $class = false !== strpos($validate, '\\') ? $validate : app()->parseClass('validate', $validate);
+
+            $v = new $class();
+
+            if (!empty($scene)) {
+                $v->scene($scene);
+            }
+        }
+
+        return $v->message($message)->batch($batch)->failException($failException);
+    }
+}
